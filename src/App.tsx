75e6a1f29e8f925b2a1081d487b2a1a61ec0778b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-13 13:17:32 +0330

    use NameForm and update prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-
 import "./App.css";
 import Counter from "./components/Counter/Counter";
+import NameForm from "./components/NameForm/NameForm";
 import NameInput from "./components/NameInput/NameInput";
 
 function App() {
- 
-
   return (
     <>
-     <div className="min-h-screen bg-gray-100 p-6 space-y-6 rounded-lg">
-      <header className="max-w-4xl mx-auto">
-        <h1 className="text-3xl font-bold">React + TypeScript + Tailwind </h1>
-      </header>
+      <div className="min-h-screen bg-gray-100 p-6 space-y-6 rounded-lg">
+        <header className="max-w-4xl mx-auto">
+          <h1 className="text-3xl font-bold">React + TypeScript + Tailwind </h1>
+        </header>
 
-      <main className="max-w-4xl mx-auto space-y-6">
-        <Counter />
-        <NameInput />
+        <main className="max-w-4xl mx-auto space-y-6">
+          <Counter />
+          <NameInput />
+          <NameForm />
         </main>
       </div>
     </>
